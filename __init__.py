7ba7305459af3219b69b9162a67d7c007adf273d
NamGyuKang <kangnamgyu27@naver.com>
2022-11-27 23:34:02 +0000

    triple backward CUDA

--- __init__.py
+++ __init__.py
@@ -1,2 +1,2 @@
-from CosineSampler.CosineSampler_2d.main import CosineSampler2d
-from CosineSampler.CosineSampler_3d.main import CosineSampler3d
+from CosineSampler_2d.main import CosineSampler2d
+from CosineSampler_3d.main import CosineSampler3d
